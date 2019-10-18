570e21b5607c46638a81b1d5cfe12facb6a87429
Clement Artaud <clement.artaud@non.se.com>
2019-10-18 17:10:10 +0200

    Use only json to configure docker daemon

--- scripts/docker-install.sh
+++ scripts/docker-install.sh
@@ -4,14 +4,6 @@ yum install -y yum-utils
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 yum install -y docker-ce docker-ce-cli containerd.io
 
-
-mkdir -p /etc/systemd/system/docker.service.d
-cat <<EOF > /etc/systemd/system/docker.service.d/10-docker-opts.conf
-[Service]
-ExecStart=
-ExecStart=/usr/bin/dockerd -H fd:// --containerd=/run/containerd/containerd.sock --iptables=false --ip-masq=false
-EOF
-
 # Setup daemon
 mkdir -p /etc/docker
 cat > /etc/docker/daemon.json <<EOF
@@ -24,7 +16,9 @@ cat > /etc/docker/daemon.json <<EOF
   "storage-driver": "overlay2",
   "storage-opts": [
     "overlay2.override_kernel_check=true"
-  ]
+  ],
+  "iptables": false,
+  "ip-masq": false
 }
 EOF
 
